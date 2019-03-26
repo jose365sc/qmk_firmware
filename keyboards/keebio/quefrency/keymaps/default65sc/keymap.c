9d1f06ea3a36efaadc9f29e040d54011b3bbb66c
jose365 <chino@shusuke.net>
2019-03-26 19:48:09 +0900

    move Fn to left most place

--- keyboards/keebio/quefrency/keymaps/default65sc/keymap.c
+++ keyboards/keebio/quefrency/keymaps/default65sc/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-    KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1),KC_SPC,           KC_SPC,  _______, MO(_FN1),KC_PSCR, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT
+    MO(_FN1),KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,           KC_SPC,  _______, MO(_FN1),KC_PSCR, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN1] = LAYOUT_65(
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-    KC_LCTL, KC_MENU, KC_LALT, _______, KC_SPC,           KC_SPC,  _______, _______, KC_NLCK, KC_INS,  KC_LEFT, KC_DOWN, KC_RGHT
+    _______, KC_LCTL, KC_APP, KC_LALT,  KC_SPC,           KC_SPC,  _______, _______, KC_NLCK, KC_INS,  KC_LEFT, KC_DOWN, KC_RGHT
   )
 
 /*   [_FN1] = LAYOUT_65(
