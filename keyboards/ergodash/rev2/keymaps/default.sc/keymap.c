2051bb9349515a5353f836ade32fb4ad1774eb53
jose365 <chino@shusuke.net>
2019-04-27 09:16:01 +0900

    my ergodash keymap

--- keyboards/ergodash/rev2/keymaps/default.sc/keymap.c
+++ keyboards/ergodash/rev2/keymaps/default.sc/keymap.c
@@ -21,19 +21,19 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(  \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    LOWER,                          LOWER,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_QUOT,                        KC_BSLS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_PSCR,                        KC_DEL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_LBRC, \
-    KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,                        _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC, \
-    KC_LCTL, KC_LGUI, KC_TAB,  KC_LALT,          KC_LCTL, KC_SPC,KC_LSFT,          KC_BSPC,KC_ENT, LOWER,            KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT  \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                        KC_EQL,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_QUOT,                        KC_BSLS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_PSCR, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,                        _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
+    LOWER,   KC_LGUI, KC_TAB,  KC_LALT,          KC_LCTL, KC_SPC,  KC_LSFT,      KC_BSPC, KC_SPC,  LOWER,            KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT  \
   ),
 
   [_LOWER] = LAYOUT(  \
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   LOWER,                          LOWER,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_MINS, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_QUOT,                        KC_BSLS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NLCK,                        KC_INS,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_LBRC, \
-    KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,                        _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC, \
-    KC_LCTL, KC_APP, KC_TAB,  KC_LALT,          KC_LCTL, KC_SPC,KC_LSFT,          KC_BSPC,KC_ENT, LOWER,             KC_HOME, KC_PGUP, KC_PGDN, KC_END  \
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_MINS,                        KC_EQL,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC, \
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_INS,  \
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_QUOT,                        KC_BSLS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_NLCK, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,                        _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
+    LOWER,   KC_APP,  KC_TAB,  KC_LALT,          KC_LCTL, KC_SPC,  KC_LSFT,      KC_BSPC, KC_SPC,  LOWER,            KC_HOME, KC_PGUP, KC_PGDN, KC_END  \
   )
 
 #if 0
